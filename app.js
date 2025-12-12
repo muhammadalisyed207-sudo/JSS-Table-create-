6e895c87541bab64e8fa5d7db61b3e9281a5e137
Syed Muhammed Ali <muhammadalisyed207@gmail.com>
2025-12-12 17:21:13 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,58 @@
+// let dice = Math.ceil(Math.random() *8)
+// console.log(dice)
+// switch(dice){
+// case 1 :console.log("one")
+// break;
+
+// case 2 :console.log("two")
+// break;
+
+// case 3 :console.log("three")
+// break;
+
+// case 4 :console.log("four")
+// break;
+
+// case 5 :console.log("five")
+// break;
+
+// case 6 :console.log("six")
+// break;
+
+// case 7:console.log("seven")
+// break;
+
+// case 8:console.log("eight")
+// break;
+
+// }
+
+
+// function table (a){
+//       for (let i = 1; i <=10; i++){
+//         console.log(a,"x",i,"=",a*i)
+//       }
+//     }
+
+// table()
+
+
+
+var popularnumber = 4;
+
+alert(2+2)
+
+var popularnumber = 12-24;
+
+// var popularnumber = 3 * 12; var num = 10;
+// var num = 10;
+// var anothernum = 1;
+// var popularnumber = num + anothernum;
+// var popularNumber = num + 200;
+
+// var leftover = 10 % 3;
+
+// var whatover = 9 % 3 ;
+// var number = 10;
+// var divide = number / 2;
+
